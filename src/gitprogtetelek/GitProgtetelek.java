7050d7b9bbe4011e6f67caedd6dfa29b1cb8fe72
younes <younes.abdalla@gmail.com>
2024-05-28 19:51:22 +0200

    osszegzes hozzadva

--- src/gitprogtetelek/GitProgtetelek.java
+++ src/gitprogtetelek/GitProgtetelek.java
@@ -7,8 +7,22 @@ public class GitProgtetelek {
     public static int[] szamok = {1,4,9,5,6};
 
     public static void main(String[] args) {
+        
+        progTetelek();
 
         
+    }
+
+    private static void progTetelek() {
+        osszegzes();
+    }
+
+    private static void osszegzes() {
+        int osszeg = 0;
+        for (int i = 0; i < szamok.length; i++) {
+            osszeg += szamok[i];
+        }
+        System.out.println("A számok osszege: " + osszeg);
     }
     
 }
