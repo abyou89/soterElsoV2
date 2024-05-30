5423ac84917b3e76cac2c4096ae1d84f2ad25785
younes <younes.abdalla@gmail.com>
2024-05-30 23:59:21 +0200

    elso negy progtetel tesztje hozzadva

--- src/teszt/tesztProgtetelek.java
+++ src/teszt/tesztProgtetelek.java
@@ -41,15 +41,34 @@ public class tesztProgtetelek {
     }
 
     private static void megszamlalasTeszt() {
-        
+        int darab = 0;
+        for (int i = 0; i < szamokTeszt.length; i++) {
+            if(szamokTeszt[i] % 2 == 0){
+                darab++;
+            }
+        }
+        assert darab == 2 : "Hiba, a paros elemek szama nem 2";
     }
 
     private static void maximumKivalasztasTeszt() {
-       
+       int maxIndex = 0;
+        for (int i = 0; i < szamokTeszt.length; i++) {
+            if(szamokTeszt[i] > szamokTeszt[maxIndex]){
+                maxIndex = i;
+            }
+        }
+        assert maxIndex == 2 : "Hiba, a legnagyobb elem nem a 2. helyen szerepel";
     }
 
     private static void minimumKivalasztasTeszt() {
-        
+        int minIndex = 0;
+        for (int i = 0; i < szamokTeszt.length; i++) {
+            if(szamokTeszt[i] < szamokTeszt[minIndex]){
+                minIndex = i;
+            }
+        }
+        System.out.println("A sorozat legkisebb eleme: " + szamokTeszt[minIndex]);
+        System.out.println("A legkisebb elem helye: " + minIndex);
     }
 
     private static void vanLegalabbEgyTeszt() {
